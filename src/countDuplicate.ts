37bbc363879a0ea1bb1b88271a04eb9055046e93
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-08-20 16:08:09 +0300

    Code runs in TS, format lint test checks

--- src/countDuplicate.ts
+++ src/countDuplicate.ts
@@ -1,11 +1,25 @@
 /**
- * Adds together two numbers
- * @param a - the first number to add
- * @param b - the second number to add
- * @returns the total
+ * Count duplicate characters of the input string
+ * @param string- the input string
+ * @returns the count of duplicate elements
  */
-function sum(a: number, b: number): number {
-  return a + b;
+function countDuplicates(string: string): number {
+  const array = string.toLowerCase().split(""); // case insensitive array
+  interface countObject {
+    [key: string]: number;
+  }
+  const count: countObject = {};
+  let duplicates = 0;
+  array.forEach(function (element: string): void {
+    count[element] = (count[element] || 0) + 1;
+  }); // make object with the element and their coutns as key-value pair
+  for (const value of Object.values(count)) {
+    // iterate over object values to find how many duplicate characters there are
+    if (value > 1) {
+      duplicates++;
+    }
+  }
+  return duplicates;
 }
 
-export default sum;
+export default countDuplicates;
